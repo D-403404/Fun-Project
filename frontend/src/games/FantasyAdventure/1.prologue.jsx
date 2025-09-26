16e91c4c6665535ed607567495d7195bbf3f36cb
NoobCode4P <qthang3004@gmail.com>
2025-09-26 10:50:17 +0700

    T: clean after test "Creating Trees as Tiled Objs"

--- frontend/src/games/FantasyAdventure/1.prologue.jsx
+++ frontend/src/games/FantasyAdventure/1.prologue.jsx
@@ -14,10 +14,14 @@ const TILEMAPS_PATH = ASSETS_ROOT_PATH + 'tilemaps/';
 // const CHAR_CARRY_PATH = RESOURCES_PATH + 'Pixel Crawler - Free Pack/Entities/Characters/Body_A/Animations/';
 
 export default class Scene1 extends Phaser.Scene{
+    /* Environment */
     tilemap;
     wallLayer;
+    treeColliders;
 
+    /* Entities */
     player;
+
     inputSystem;
     debugGraphics;
 
@@ -36,142 +40,32 @@ export default class Scene1 extends Phaser.Scene{
     }
 
     create(){
-        // Create a tilemap with different layers
-        // this.tilemap = this.make.tilemap({key:'map01'});
-        // const floorTileset = this.tilemap.addTilesetImage('FloorTileset', 'floor_tiles');
-        // const wallTileset = this.tilemap.addTilesetImage('WallTileset', 'wall_tiles');
-
-        // const floorLayer = this.tilemap.createLayer('Floor', floorTileset);
-        // const wallLayer = this.tilemap.createLayer('Wall', wallTileset);
-
-        // // console.log(tetLayer)
-        // console.log(this.tilemap)
-        // // let treeLayer = this.make.tilemap({key:'map01'}).createLayer(1, 'Trees').setCollisionFromCollisionGroup(true);
-        // wallLayer.setCollisionByProperty({ collision: true });
-        // treeLayer.setCollisionFromCollisionGroup();
-
         this.debugGraphics = this.add.graphics();
-        this.debugGraphics.setDepth(9999999999);    // Render on top of everything else
+        this.debugGraphics.setDepth(999999);    // Render on top of everything else
+            
+        // Create tilemap
         this.createTilemap();
-
-        // Create Input System
-        this.inputSystem = new InputSystem(this);
 
         // debugTilemapCollider(this.debugGraphics, this.wallLayer);
         
+        // Create Input System
+        this.inputSystem = new InputSystem(this);
+        
+        // Create Player
         this.player = new Player(
             this, {x: 100, y: 350}, "Swordfucker-idle", 
             {x:2, y:2}, {x: 0.5, y: 0.7}, 
             new ColliderData(0.5, 0.2, 0, 20)
         );
-        // Create Animations
         createSwordfuckerAnims(this.player.anims);
         this.player.anims.play('idle');
-
-        // console.log('Here', this.make.tilemap({key:'map01'}).createFromObjects('Trees', {
-        //     gid: 1276,
-        //     key: 'trees_spritesheet_02_03'
-        // }))
-        
-        // let trees = this.tilemap.createFromObjects('Trees', {
-        //     // gid: 1276,
-        //     key:'trees'
-        // })
-
-        // trees.forEach(tree => {
-        //     // console.log('Here', tree)
-        //     this.physics.add.existing(tree, true);
-        //     // this.physics.world.enable(tree);
- 
-        //     this.physics.add.collider(this.player, tree);
-
-        // })
-
-        // const trees = this.physics.add.staticGroup();
-        // const treesLayer = this.tilemap.getObjectLayer('Trees');
-        // let depths = new Map();
-        // console.log('Trees Layer', treesLayer)
-        // treesLayer.objects.forEach(tree => {
-        //     // trees.get(tree.x, tree.y, 'trees')
-
-        //     // let t = this.physics.add.sprite(tree.x, tree.y, 'trees');
-        //     // t.setScale(3,3);
-        //     // t.body.setSize(tree.width, tree.height);
-
-        //     let gameObject = this.add.rectangle( tree.x, tree.y, tree.width, tree.height );
-
-        //     if (tree.gid){
-        //         depths.set(tree.id, tree.y);
-        //         gameObject.setOrigin(0, 1);
-        //     }
-
-        //     else if (tree.ellipse){
-        //         gameObject.setOrigin(0, 0);
-        //     }
-
-
-        //     // let gameObject = this.add.ellipse( tree.x, tree.y, tree.width, tree.height )
-        //     //     .setOrigin(0, 1);
-        
-        //     if (!tree.gid){
-        //         this.physics.add.existing( gameObject, true );
-        //         this.physics.add.collider(this.player, gameObject );
-        //     }
-        //     if (tree.properties){
-        //         console.log(tree.properties)
-        //         // console.log(typeof(tree.properties[0]))
-        //         // tree.properties.forEach(e => console.log(e))
-        //         console.log(tree.properties.find(e => e.name==='age'))
-        //     }
-            
-        //     // if(tree.ellipse){
-        //     // // For the ellipse version you would need to change the body
-        //     //     gameObject.body.setCircle( tree.width / 2 );
-        //     //     // gameObject = this.add.ellipse(tree.x, tree.y, tree.width, tree.height);
-
-        //     // } else if(tree.point){
-        //     //     // For the point we need no set an width and height
-        //     //     gameObject.body.setSize( 4, 4 );
-        //     // }
-        //     debugPoints(this.debugGraphics, tree.x, tree.y);
-        //     // this.physics.add.collider(this.player, gameObject );
-        // })
-
-        // let firstGid = this.tilemap.tilesets[2].firstgid;
-        // let spriteArr = this.tilemap.createFromObjects('Trees', {
-        //     gid: 1278,
-        //     key: 'trees_spritesheet_02_03',
-        //     frame: 1278 - firstGid
-        // });
-        // // spriteArr.forEach((item, idx)=>{
-        // //     console.log(item)
-        // // })
-        // this.tilemap.createFromObjects('Trees', {
-        //     id: 68,
-        //     key: 'trees_spritesheet_02_03',
-        //     frame: 1276 - firstGid
-        // })[0].setDepth(1);
-
-        // this.tilemap.createFromObjects('Trees', {
-        //     id: 72,
-        //     key: 'trees_spritesheet_02_03',
-        //     frame: 1276 - firstGid
-        // })[0].setDepth(10);
-
-        // // console.log(spriteArr);
-        // console.log(depths)
 
         // ======== Physics config ========= //
         this.physics.world.setBounds(0, 0, this.tilemap.widthInPixels, this.tilemap.heightInPixels);
         this.player.setCollideWorldBounds(true);
 
         // Create Collision Matrix
-        this.physics.add.collider(this.player, this.wallLayer);
-        // this.physics.add.collider(this.player, tetLayer, (o1, o2)=>{
-        //     console.log(o1)
-        //     debugBox(this.debugGraphics, o2.pixelX, o2.pixelY, o2.width, o2.height)
-        //     console.log(o2)
-        // });
+        this.setupCollisionMatrix();
         
         this.setupCamera();
     }
@@ -190,10 +84,9 @@ export default class Scene1 extends Phaser.Scene{
 
         const treeLayer = tilemap.getObjectLayer('Trees');
         
-        // console.log(tilemap)
-        console.log(treeLayer.objects)
+        this.treeColliders = this.physics.add.staticGroup();
 
-        let treeObjTileGIDs = new Set();
+        let treeObjTileGIDs = new Set();    // Get all tile sprites'gids
 
         /*
             This is used for sorting sprites layer
@@ -207,7 +100,7 @@ export default class Scene1 extends Phaser.Scene{
                 Meaning a list will be [GO whose id=4, GO whose id=7,....] even though GO doesn't store `id`
         */
         let yCoordsMap = new Map();
-        
+
         treeLayer.objects.forEach(obj => {
             if (obj.gid){   // Tiled Object
 
@@ -228,8 +121,9 @@ export default class Scene1 extends Phaser.Scene{
                             // or
                             colliderGO.setPosition(collider.x + collider.width/2, collider.y + collider.height/2);
 
-                            this.physics.add.existing(colliderGO, true);
-                            // colliderGO.body.setCircle(Math.max(collider.width, collider.height)/2)
+                            // colliderGO.body.setCircle(Math.max(collider.width, collider.height)/2);
+
+                            this.treeColliders.add(colliderGO);
                         }
                     }
                 }
@@ -241,14 +135,8 @@ export default class Scene1 extends Phaser.Scene{
             }
         });
 
-        console.log(yCoordsMap)
-
         let firstGid = tilemap.getTileset('Trees_03').firstgid;
-        let color = [Color.BLUE, Color.YELLOW];
-        let i = 0;
 
-        debugPoints(this.debugGraphics, 235.53, 278.67, Color.LIME, 10);
-        debugPoints(this.debugGraphics, 244.67, 123.33, Color.LIME, 10);
         for (let gid of treeObjTileGIDs){
             let sprites = tilemap.createFromObjects('Trees', {
                 gid: gid,
@@ -257,7 +145,6 @@ export default class Scene1 extends Phaser.Scene{
             })
             let yCoords = yCoordsMap.get(gid);
 
-            console.log(sprites)
             sprites.forEach((value, index)=>{
                 let zDepth = yCoords[index];
                 value.setDepth(zDepth);
@@ -268,10 +155,7 @@ export default class Scene1 extends Phaser.Scene{
                 // However, in 2D RGP, its origin is preferred to set to near center bottom
                 value.setOrigin(0.5, 0.85);
                 value.setPosition(value.x, value.y + value.displayHeight*(0.85-0.5));
-
-                debugPoints(this.debugGraphics, value.x, value.y, color[i], i+5)
             })
-            i++;
         }
         
         /*
@@ -287,59 +171,16 @@ export default class Scene1 extends Phaser.Scene{
             ```
             - But the sprites (of tiled object) are unresizable like other ordinary game objects
         */
-
-        // const tetLayer = this.make.tilemap({key:'map01'})
-        //     .createLayer('Tile Layer 3', tetTileset)
-        //     .setCollisionFromCollisionGroup();
-        // tetLayer.forEachTile(tile => {
-        //     const tileWorldX = tile.getLeft();
-        //     const tileWorldY = tile.getTop();
-        //     const collisionGroup = tile.getCollisionGroup();
-            
-        //     if (!collisionGroup || collisionGroup.objects.length === 0) {
-        //         return; // Skip if no collision data
-        //     }
-        //     console.log(collisionGroup)
-        //     console.log(tile)
-        //     tile.setSize(tile.width*20, tile.height*20, tile.baseHeight, tile.baseWidth);
-
-        //     // Iterate through the collision objects for this tile
-        //     collisionGroup.objects.forEach(obj => {
-        //         const objectX = tileWorldX + obj.x;
-        //         const objectY = tileWorldY + obj.y;
-        //         const objectWidth = obj.width;
-        //         const objectHeight = obj.height;
-
-        //         // Set the color for the collision shape
-        //         // this.debugGraphics.lineStyle(2, 0x00ff00); // Green lines for visualization
-        //         // this.debugGraphics.strokeRect(objectX, objectY, objectWidth, objectHeight);
-
-        //         let shape;
-        //         if (obj.ellipse) {
-        //             shape = this.add.ellipse(objectX + obj.width/2, objectY + obj.height/2, obj.width, obj.height);
-        //         } else {
-        //             shape = this.add.rectangle(objectX + obj.width/2, objectY + obj.height/2, obj.width, obj.height);
-        //         }
-
-        //         // shape.setScale(2,2)
-
-        //         // Make it invisible but collidable
-        //         shape.setVisible(true);
-
-        //         // console.log('Here', obj)
-        //         this.physics.add.existing(shape, true)
-        //         this.physics.add.collider(this.player, shape, (o1, o2)=>{
-        //             console.log('O1', o1)
-        //             debugBox(this.debugGraphics, o2.pixelX, o2.pixelY, o2.width, o2.height)
-        //             console.log('O2', o2)
-        //         });
-        //     });
-        // });
     }
     
     update(){
         // this.debugGraphics.clear();
         this.player.update();
+    }
+
+    setupCollisionMatrix(){
+        this.physics.add.collider(this.player, this.wallLayer);
+        this.physics.add.collider(this.player, this.treeColliders);
     }
 
     setupCamera(){
--- frontend/src/pages/T.jsx
+++ frontend/src/pages/T.jsx
@@ -4,16 +4,12 @@ export default function T(){
     let config = {
         type: Phaser.AUTO,
         // Allows Phaser canvas to be responsive to browser sizing
-        // scale: {
-        //     mode: Phaser.Scale.ENVELOP,
-        //     mode: Phaser.Scale.FIT,
-        //     autoCenter: Phaser.Scale.CENTER_BOTH,
-        //     // width: 1500,
-        //     // height: 1000,
-        //     // parent: 'game-container',
-        // },
-        width: 800,
-        height: 1000,
+        scale: {
+            mode: Phaser.Scale.RESIZE,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+        },
+        width: window.innerWidth,
+        height: window.innerHeight,
         physics: {
             default: 'arcade',
             arcade: {
